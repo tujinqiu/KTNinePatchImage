ee0c1ab688e55975add069f73ce17c109fa785f5
Kevin Tu <819931323@qq.com>
2018-11-11 12:44:24 +0800

    Initial Commit

---
+++ KTNinePatchImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KTNinePatchImage
+//
+//  Created by tu on 2018/11/11.
+//  Copyright © 2018年 tu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KTNinePatchImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KTNinePatchImage
+//
+//  Created by tu on 2018/11/11.
+//  Copyright © 2018年 tu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
